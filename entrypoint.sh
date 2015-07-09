b83c533b6f1fb3552fa122be0daa7736d04893f4
Martin Mauch <mauch@crealytics.de>
2015-07-09 20:06:22 +0200

    Wait for linked containers to come up before running sqitch

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,17 @@
-#!/bin/sh
+#!/bin/bash
+host=$(env | grep _TCP_ADDR | head -1 | cut -d = -f 2)
+port=$(env | grep _TCP_PORT | head -1 | cut -d = -f 2)
+
+echo -n "waiting for TCP connection to $host:$port..."
+
+while ! ((echo > /dev/tcp/$host/$port) >/dev/null 2>&1)
+do
+  echo -n .
+  sleep 1
+done
+
+echo "Got connection"
+
 sqitch "$@"
 
 # $LOCALUID, $LOCALGID are env variables set by docker run
